8b2dc1a8534714ae2958bc0022f3d683e6307aa1
psychesworld <psychesworld@gmail.com>
2017-01-28 21:49:13 +0800

    Add get method and post method for book

--- app/book/models.py
+++ app/book/models.py
@@ -11,6 +11,23 @@ class Book(db.Model):
     publication_date = db.Column(db.Date)
     original_price = db.Column(db.Numeric(10, 2))
 
+    def __init__(self, name=None, author=None, press=None, publication_date=None, original_price=None):
+        self.name = name
+        self.author = author
+        self.press = press
+        self.publication_date = publication_date
+        self.original_price = original_price
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'author': self.author,
+            'press': self.press,
+            'publication_date': self.publication_date,
+            'original_price': self.original_price
+        }
+
     def __str__(self):
         return '<"%s" by %s | %s | %s>' % (
             self.name, self.author, self.press, self.publication_date)
@@ -25,6 +42,12 @@ class BookExpectCondition(db.Model):
     expect_price = db.Column(db.Numeric(10, 2))
     expect_quality = db.Column(db.String(16))
 
+    def __init__(self, book_id, if_on_sell=False, expect_price=None, expect_quality=None):
+        self.book_id = book_id
+        self.if_on_sell = if_on_sell
+        self.expect_price = expect_price
+        self.expect_quality = expect_quality
+
 
 class BookSalesInfo(db.Model):
     __tablename__ = 'BookSalesInfos'
@@ -36,3 +59,10 @@ class BookSalesInfo(db.Model):
     quality = db.Column(db.String(16))
     location = db.Column(db.String(128))
     created_on = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __init__(self, book_id, url, sale_price=None, quality=None, location=None):
+        self.book_id = book_id
+        self.url = url
+        self.sale_price = sale_price
+        self.quality = quality
+        self.location = location
--- app/book/views.py
+++ app/book/views.py
@@ -1,12 +1,37 @@
 from . import book
+from .models import Book, BookExpectCondition
+from app import db
+from flask import request, jsonify
 
 
 @book.route('/book', methods=['POST'])
-def add():
-    pass
+def add_book():
+    name = request.json.get('name')
+    author = request.json.get('author')
+    press = request.json.get('press')
+    publication_date = request.json.get('publication_date')
+    original_price = request.json.get('original_price')
+    new_book = Book(name=name, author=author, press=press, publication_date=publication_date,
+                    original_price=original_price)
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({'msg': '添加成功'}), 201
 
 
-@book.route('/book/<int:id>', methods=['GET'])
-def get_by_id(id):
-    pass
+@book.route('/book', methods=['GET'])
+def get_all():
+    books = Book.query.all()
+    return jsonify(json_list=[item.serialize for item in books]), 200
 
+
+@book.route('/book_expect_condition', methods=['POST'])
+def add_book_expect_condition():
+    book_id = request.json.get('book_id')
+    if_on_sell = request.json.get('if_on_sell', False)
+    expect_price = request.json.get('expect_price')
+    expect_quality = request.json.get('expect_quality')
+    new_book_expect_condition = BookExpectCondition(book_id, if_on_sell=if_on_sell, expect_price=expect_price,
+                                                    expect_quality=expect_quality)
+    db.session.add(new_book_expect_condition)
+    db.session.commit()
+    return jsonify({'msg': '添加成功'}), 201
